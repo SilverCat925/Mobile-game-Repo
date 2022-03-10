7ab71ef05798fd1b3f06a5fb80fa686309b5eb59
SilverCat925 <94045975+SilverCat925@users.noreply.github.com>
2022-03-10 10:48:59 +0000

    1 created HelloWorld

---
+++ mobile game Unity/Assets/Scripts/HelloWorld.cs
@@ -0,0 +1,31 @@
+// HelloWorld
+// by Josh A Agnew
+// 10/3/2022
+
+//-------------------------------------------------------
+//libraries used
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+//--------------------------------------------------------
+
+public class HelloWorld : MonoBehaviour
+{
+    //--------------------------------------------------
+    // Start is called before the first frame update
+    void Start()
+    {
+        Debug.LogWarning("Cats");
+    }
+    //--------------------------------------------------
+
+    //--------------------------------------------------
+    // Update is called once per frame
+    void Update()
+    {
+        Debug.LogError("MeoM");
+    }
+    //--------------------------------------------------
+}
+/// Closing bracket
+///------------------------------------------------------------------------
